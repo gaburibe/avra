5183417155d1beff8182ec49a933ff6b87def60e
Gabriel <gaburibe@gmail.com>
2026-04-07 17:36:36 -0600

    fix init_db para gunicorn

--- app.py
+++ app.py
@@ -82,7 +82,8 @@ def save_voz():
     conn.close()
     return jsonify({'ok': True})
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
